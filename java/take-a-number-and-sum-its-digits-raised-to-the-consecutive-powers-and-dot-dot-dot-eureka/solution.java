115731747db17390d8c4921b79e0c044a1789edf
Kriksson <k.kriksson@gmail.com>
2026-09-06 20:54:53 +0300

    Solve Take a Number And Sum  Its Digits Raised To The Consecutive Powers And ....¡Eureka!! [java]

--- java/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/solution.java
+++ java/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-dot-dot-dot-eureka/solution.java
@@ -7,16 +7,16 @@ class SumDigPower {
     
     public static List<Long> sumDigPow(long a, long b) {
         List<Long> answer = new ArrayList<>();
-        for (int i = (int) a; i <= (int) b; i++) {
-            char[] nums = String.valueOf(i).toCharArray();
-            int sum = 0;
-            for (int j = 0; j < nums.length; j++) {;
-                int num = Integer.parseInt(String.valueOf(nums[j]));
-                int plus = (int) Math.pow(num, j+1);
+        for (long number = a; number <= b; number++) {
+            char[] digits = String.valueOf(number).toCharArray();
+            long sum = 0;
+            for (int j = 0; j < digits.length; j++) {
+                int digit = digits[j] - '0';
+                int plus = (int) Math.pow(digit, j+1);
                 sum = sum + plus;
             }
-            if (sum == i) {
-                answer.add((long) i);
+            if (sum == number) {
+                answer.add(number);
             };
         }
         return answer;
