d1175b5f01127ed2621e37be4c7db693a241c662
Kriksson <k.kriksson@gmail.com>
2026-09-06 19:46:50 +0300

    Solve Your order,  please [java]

--- java/your-order-please/solution.java
+++ java/your-order-please/solution.java
@@ -5,8 +5,11 @@ import java.util.regex.Pattern;
 
 public class Order {
   public static String order(String words) {
+    if (words.trim().isEmpty()) {
+      return "";
+    }
     String[] wordsArray = words.trim().split("\\s+");
-    String[] newWordsArray = words.trim().split("\\s+");
+    String[] newWordsArray = new String[wordsArray.length];
     for (String word : wordsArray) {
       int numberOfWord = 0;
       Matcher m = Pattern.compile("\\d+").matcher(word);
