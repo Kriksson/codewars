914b84ebea62bf54a96918efabf6fbc77bfbec9e
Kriksson <k.kriksson@gmail.com>
2026-09-08 14:02:58 +0300

    Solve Mean Square Error [java]

---
+++ java/mean-square-error/solution.java
@@ -0,0 +1,12 @@
+package codewars.java.mean.square.error;
+
+
+public class Solution {
+  public static double solution(int[] arr1, int[] arr2) {
+    double sum = 0;
+    for (int i = 0; i < arr1.length; i++) {
+      sum = sum + Math.pow(arr2[i] - arr1[i], 2);
+    }
+    return sum / arr1.length;
+  }
+}
