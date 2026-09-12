dc589af2de848346f6d0494f5b8178898f2d29ac
Kriksson <k.kriksson@gmail.com>
2026-09-12 08:51:36 +0300

    Solve Keep up the hoop [java]

---
+++ java/keep-up-the-hoop/solution.java
@@ -0,0 +1,7 @@
+package codewars.java.keep.up.the.hoop;
+
+public class HelpAlex{
+  public static String hoopCount(int n){
+        if (n >= 10) { return "Great, now move on to tricks"; } else { return "Keep at it until you get it"; }
+  }
+}
