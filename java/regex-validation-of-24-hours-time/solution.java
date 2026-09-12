4747c1667257bddabc810c945644619d6815dc2e
Kriksson <k.kriksson@gmail.com>
2026-09-12 16:54:59 +0300

    Solve regex  validation of 24 hours time. [java]

---
+++ java/regex-validation-of-24-hours-time/solution.java
@@ -0,0 +1,8 @@
+package codewars.java.regex.validation.of._24.hours.time;
+
+import java.util.regex.*;
+
+public class RegexValidation {
+    // assign your regexp to this class constant:
+    public static final Pattern TIME_PATTERN = Pattern.compile("^([01]?[0-9]|2[0-3]):[0-5][0-9]$");
+}
