ddc7a8c9e13c7bedb095281c64bf5c759473611f
Kriksson <k.kriksson@gmail.com>
2026-09-12 09:28:33 +0300

    Solve Reversed Strings [java]

---
+++ java/reversed-strings/solution.java
@@ -0,0 +1,9 @@
+package codewars.java.reversed.strings;
+
+public class Kata {
+
+  public static String solution(String str) {
+    return new StringBuilder().append(str).reverse().toString();
+  }
+
+}
