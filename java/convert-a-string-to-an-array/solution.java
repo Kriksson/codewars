69daf20a552a4d3d3a5431f90ad2529365133f1e
Kriksson <k.kriksson@gmail.com>
2026-09-12 08:44:25 +0300

    Solve Convert a string to an array [java]

---
+++ java/convert-a-string-to-an-array/solution.java
@@ -0,0 +1,12 @@
+package codewars.java.convert.a.string.to.an.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+    public static String[] stringToArray(String s) {
+      return s.split(" ");
+    }
+
+}
